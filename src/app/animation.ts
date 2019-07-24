234ff2cfc32e4de872f7e22b1c6634d76e19969d
PasangDimdung <pasangdimdung045@gmail.com>
2019-07-24 14:11:13 +0545

    created reuseable triggers and animations

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -1,4 +1,20 @@
-import { trigger, transition, animate, style, state, keyframes } from '@angular/animations';
+import { trigger, transition, animate, style, state, keyframes, animation, useAnimation } from '@angular/animations';
+
+export let bounceOutAnimation = animation(
+    animate('500ms cubic-bezier(.5,1.84,.03,-0.69)', keyframes([
+        style({
+            offset: .2,
+            opacity: 1,
+            transform: 'translateX(20px)'
+        }),
+
+        style({
+            offset: 1,
+            opacity: 0,
+            transform: 'translateX(-100%)'
+        })
+    ]))
+)
 
 export let slide = trigger('slide', [
     transition('void=>*',[
@@ -7,26 +23,28 @@ export let slide = trigger('slide', [
     ]),
 
     transition('*=>void',[
-        animate('500ms cubic-bezier(.5,1.84,.03,-0.69)', keyframes([
-            style({
-                offset: .2,
-                opacity: 1,
-                transform: 'translateX(20px)'
-            }),
-
-            style({
-                offset: 1,
-                opacity: 0,
-                transform: 'translateX(-100%)'
-            })
-        ]))
+        useAnimation(bounceOutAnimation)
     ])
 ]);
 
-// export let fade = trigger('fade', [
-//     state('void', style({opacity: 0})),
+export let fadeInAnimation = animation([
+    style({opacity:0}),
+    animate('{{ duration }} {{ easing }}')
+], 
+{
+    params:{ duration: '2000ms', easing: 'ease-out' }
+}
+)
 
-//     transition('void <=> *', [
-//       animate(2000)
-//     ])
-//   ]);
+export let fadeOutAnimation = animation([
+    animate(2000, style({opacity:0}))
+])
+
+export let fade = trigger('fade', [
+    transition('void=>*',[
+       useAnimation(fadeInAnimation)
+    ]),
+    transition('*=>void',[
+       useAnimation(fadeOutAnimation)
+      ])
+  ]);
